40a18d8e1fe501948bf7d1d6adcf14a2d653b164
Matias Gonzalez <mng_epson@hotmail.com>
2023-07-02 22:47:19 -0300

    feat[add methods preflight]

---
+++ Parte7/server/server.js
@@ -0,0 +1,25 @@
+//practica de cors aceptando todos los origenes
+
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+
+app.use(morgan('tiny'))
+
+let f = (req, res)=>{
+  res.header("Access-Control-Allow-Origin","*")
+  res.send({
+    hola:"mundo"
+  });
+}
+
+app.get("/", f);
+
+app.post("/", f);
+app.put("/", f);
+
+app.listen(8080, ()=>console.log("Server started"))
---
+++ Parte9/server/server.js
@@ -0,0 +1,32 @@
+//CONFIGURAR METODOS PREFLIGHT methods, headers, content type y authentication
+
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+
+app.use(morgan('tiny'))
+
+let f = (req, res)=>{
+  res.header("Access-Control-Allow-Origin","*")
+  res.send({
+    hola:"mundo"
+  });
+}
+
+app.options("/", (req, res)=>{
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Methods","PUT");
+  res.header("Access-Control-Allow-Headers","Content-Type, Authentication")
+  res.status(204).send()
+})
+
+app.get("/", f);
+
+app.post("/", f);
+app.put("/", f);
+
+app.listen(8080, ()=>console.log("Server started"))
